63847d0929f8562a1fc6f20936099c002ae920a5
matute005 <96752247+matute005@users.noreply.github.com>
2021-12-27 20:39:46 -0300

    Create pruebaa.py

---
+++ pruebaa.py
@@ -0,0 +1,8 @@
+inputVec = input()
+inputVec = inputVec.split()
+x = inputVec[0]
+y = inputVec[1]
+z = inputVec[2]
+print("el valor de es x "+ x)
+print("el valor de es y "+ y)
+print("el valor de es z "+ z)
